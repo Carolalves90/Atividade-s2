2b0c99eb8842ad1dc97b84ead8f46ca65bb760b6
carolalves <prof.carolalves90@gmail.com>
2022-04-04 23:18:01 -0300

    fazendo exercícios 2 e 3

--- Exerc2.js
+++ Exerc2.js
@@ -1,1 +1,7 @@
 //Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distancia = undefined
+let velocmed = undefined
+let tempoviagem = distancia * velocmed
+
+console.log (`O tempo de viagem é ${tempoviagem}`)
--- Exerc3.js
+++ Exerc3.js
@@ -1,2 +1,7 @@
 // Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
 
+let valorjantar = undefined
+let taxagarcom = 10/100
+let totaljantar = valorjantar + taxagarcom
+
+console.log (`O valor a pagar pelo jantar é ${totaljantar}`)
