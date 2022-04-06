66f2e12b629ee1d1388c3e476dbff723c824d6c5
carolalves <prof.carolalves90@gmail.com>
2022-04-06 12:34:18 -0300

    concluindo ultimo exerc

--- Exerc4.js
+++ Exerc4.js
@@ -2,11 +2,11 @@
 //assim como a quantidade de dias pelos quais o carro foi alugado. 
 //Calcule o preço a pagar, sabendo que o carro custa 60,00 reais por dia e 0,15 centavos por km rodado.
 
-let quantidadeKm =
-let quatindadeDias =
-let custoCarro = 60
-let custoKm = 0,15
+let quantidadeKm = 560
+let quatindadeDias = 7
+let custoCarro = 60 * quatindadeDias
+let custoKm = 0.15 * quantidadeKm
 
-const valorAluguel = 
+const valorAluguel = custoCarro + custoKm
 
 console.log(`O preço a pagar pelo aluguel do carro é ${valorAluguel}`)
